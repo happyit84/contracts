618ba951c696dff860c02fed2344a07c0d38f525
"happyit84" <"choninchonin@gmail.com">
2022-02-12 13:23:06 +0900

    add test.sol

---
+++ LockManager.sol
@@ -0,0 +1,36 @@
+ // SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract LockerTest is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+
+    constructor() ERC721("LockerTestNFT", "LTN") {}
+
+    mapping(uint => string) tokenURIs;
+
+    function _baseURI() internal view override returns (string memory) {
+        return "ipfs://";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        return string(abi.encodePacked(_baseURI(), tokenURIs[tokenId]));
+    }
+
+    function mintToken(address owner, string memory metadataHash) public onlyOwner returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(owner, newItemId);
+        tokenURIs[newItemId] = metadataHash;
+        return newItemId;
+    }
+}
